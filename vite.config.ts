39f54b8c1f91083c7c265d722a57b56145717ccc
struggle <motoyatiago@gmail.com>
2026-09-24 12:56:46 +0800

    Update SITE_CREATOR_PLACEHOLDER_DATABASE_ID

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "84d0294f-dbef-4169-ab9b-34a3bcc38758";
 
 const { d1, r2 } = hostingConfig;
 
